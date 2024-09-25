22632645240e405afc2a0230879f88af34127b89
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-09-25 13:12:07 -0400

    nueva version en astro

--- src/components/LinkWithIcon.tsx
+++
@@ -1,9 +0,0 @@
-function LinkWithIcon({link, icon}:LinkWithIcon) {
-    return(
-        <a href={link} target="_blank">
-            <img src={icon} alt="repositori" />
-        </a>
-    );
-}
-export default LinkWithIcon;
-export { LinkWithIcon };
