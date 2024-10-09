318518d79bc2fff2700ebd41b80ecd36a8352475
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-10-09 03:56:02 -0400

    nuevo diseño a vanzado

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist: [
+		{
+			pattern: /border-(red|blue|green|yellow|purple|pink|orange)-(900|600|500|400|300|200|100)/,
+			variants: ['hover', 'before']
+		},
+		{
+			pattern: /bg-(red|blue|green|yellow|purple|pink|orange)-(900|600|500|400|300|200|100)/,
+			variants: ['hover', 'before']
+		}
+	],
 	theme: {
 		extend: {},
 	},
