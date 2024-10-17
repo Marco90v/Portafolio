ff75a653ee465a7d598bf77009bdbbe6b7b8090c
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-10-17 06:47:46 -0400

    feat: mejoras en los estilos y optimizacion de algunas imagenes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	safelist: [
 		{
+			pattern: /via-./,
+			variants: ['hover', 'before']
+		},
+		{
 			pattern: /border-(red|blue|green|yellow|purple|pink|orange)-(900|600|500|400|300|200|100)/,
 			variants: ['hover', 'before']
 		},
