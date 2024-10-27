ff112e8f51e96b3cac39da6e9b0f009cd233aa80
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-10-27 07:49:41 -0400

    feat: ajustes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
@@ -9,22 +9,11 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-
-  env:{
-    schema:{
-      PUBLIC_SERVICE_ID: envField.string({context:"client", access: "public", optional: false}),
-      PUBLIC_TEMPLATE_ID: envField.string({context:"client", access: "public", optional: false}),
-      PUBLIC_KEY: envField.string({context:"client", access: "public", optional: false}),
-    }
-  },
-
   build: {
     inlineStylesheets: 'always'
   },
-
   image: {
     remotePatterns: [{ protocol: "https://raw.githubusercontent.com/Marco90v/**", hostname: "raw.githubusercontent.com" }],
   },
-
   adapter: netlify()
 });
