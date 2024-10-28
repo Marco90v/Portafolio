f1937493075bb9f30a98433cf51d5a8c0a5caea0
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-10-28 15:37:19 -0400

    fix: cambio minimo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import netlify from "@astrojs/netlify/";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
